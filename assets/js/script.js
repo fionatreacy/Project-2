dc71f502eaadcfa6ed468d43e542d2bedb036a12
fionatreacy <fionatreacy00@gmail.com>
2022-03-08 12:06:56 +0000

    add computer choice, comparison and animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const game = () => {
     let pScore = 0;
     let cScore = 0; 
 
-    // start game
+    // starting the game
   const startGame = () => {
     const playBtn = document.querySelector(".intro button");
     const introScreen = document.querySelector(".intro");
@@ -11,5 +11,40 @@ const game = () => {
     playBtn.addEventListener("click", () => {
       introScreen.classList.add("fadeOut");
       match.classList.add("fadeIn");
+    });
+  };
+
+  // playing the game
+  const playMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
+    const hands = document.querySelectorAll(".hands img");
+
+    hands.forEach(hand => {
+      hand.addEventListener("animationend", function() {
+        this.style.animation = "";
+      });
+    });
+
+    // computers options and choice
+    const computerOptions = ["rock", "paper", "scissors"];
+
+    options.forEach(option => {
+      option.addEventListener("click", function() {
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+        setTimeout(() => {
+
+            // comparing choices
+            compareHands(this.textContent, computerChoice);
+            playerHand.src = `assets/images/${this.textContent}.png`;
+            computerHand.src = `assets/images/${computerChoice}.png`;
+          }, 2000);
+
+          // animation
+          playerHand.style.animation = "shakePlayer 2s ease";
+        computerHand.style.animation = "shakeComputer 2s ease";
+      });
     });
   };
